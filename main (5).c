c2413dbc06e833a555752da650dc481ded0b8e26
Beacapi <124038376+Beacapi@users.noreply.github.com>
2023-02-01 16:59:34 +0100

    Add files via upload

--- main (5).c
+++ main (5).c
@@ -1,31 +1,29 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
-int main()
-{
-    int lato1;
-    int lato2;
-    int lato3;
-    printf ("inserire tre lati");
-    scanf ("%d%d%d", &lato1,&lato2,&lato3);
+void fattoriale (int numero) {}
+
+int main ()
+    { 
+        int N;
+        int tot=1;
+        
+        printf ("prendi un numero \n");
+        scanf ("%d", &N);
+        
+        while (N>=1) {
+        tot=tot*N;
+        N=N-1;
+        }
+        printf ("finale %d\n", tot);
     
-    if (lato2==lato3 && lato1==lato2 && lato3==lato1) {
-    printf ("il triangolo e equilatero");
-    }
-    else if (lato1!=lato2 && lato2!=lato3 && lato3!=lato1) {
-    printf ("il triangolo e scaleno");
-    }
     
-    else {
-        printf ("il triangolo e isoscele");
-    }
-    
-  return 0;
+    return 0;
 }
