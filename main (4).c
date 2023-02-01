fb80b90def80428b54e7c7aa6bbdcfa94149e600
Beacapi <124038376+Beacapi@users.noreply.github.com>
2023-02-01 12:48:13 +0100

    Add files via upload

--- main (4).c
+++ main (4).c
@@ -1,30 +1,32 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
+void ft_ft (int*nbr, int*x, int y)
+{
+    *nbr=42;
+    *x=10;
+    y=102;
+    printf ("stampare il valore effettivo di y nella funzione ft_ft: %d\n",y);
+}
 int main()
 {
-    int lato1;
-    int lato2;
-    int lato3;
-    printf ("inserire tre lati");
-    scanf ("%d%d%d", &lato1,&lato2,&lato3);
+    int nbr=1;
+    int x=2;
+    int y=3;
+   
     
-    if (lato1+lato2 >lato3 && lato2+lato3>lato1 && lato3+lato1>lato2) {
-    printf ("lati possono essere le lunghezze dei lati di un triangolo");
-    }
-    else {
-    printf ("lati non possono essere le lungheze dei lati di un triangolo");
-    }
+    printf ("il valore inziale: %d\n%d\n%d\n",nbr,x,y);
+    ft_ft (&nbr,&x,y);
+    printf ("il valore effettivo dopo aver richiamato ft_ft: %d\n%d\n%d\n",nbr,x,y);
     
     
-    
-    
-  return 0;
+
+    return 0;
 }
