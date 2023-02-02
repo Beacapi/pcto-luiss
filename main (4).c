44ff46b7e13a8eca91e51c4a898bb441a8377f5c
Beacapi <124038376+Beacapi@users.noreply.github.com>
2023-02-02 12:08:45 +0100

    Add files via upload

--- main (4).c
+++ main (4).c
@@ -8,25 +8,34 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 #include <stdio.h>
 
-void ft_ft (int*nbr, int*x, int y)
-{
-    *nbr=42;
-    *x=10;
-    y=102;
-    printf ("stampare il valore effettivo di y nella funzione ft_ft: %d\n",y);
+int ft_ft(int n, int potenza)
+{  
+    int risultato;
+    risultato=1;
+    while (potenza>0) {
+    risultato=risultato*n; 
+    potenza=potenza-1; }
+    
+    if (potenza<0) {
+         printf ("risultato non agibile\n");
+         return 0;
+    }
+    return risultato;
+        
 }
-int main()
-{
-    int nbr=1;
-    int x=2;
-    int y=3;
+    
+int main ()   
+{   int n;
+    int potenza;
+    
    
+    printf ("inserire un n da elevare alla potenza\n");
+    scanf ("%d",&n);
+    printf ("inserire una potenza\n");
+    scanf ("%d",&potenza);
+    printf ("risultato è %d\n", ft_ft (n, potenza));
     
-    printf ("il valore inziale: %d\n%d\n%d\n",nbr,x,y);
-    ft_ft (&nbr,&x,y);
-    printf ("il valore effettivo dopo aver richiamato ft_ft: %d\n%d\n%d\n",nbr,x,y);
     
-    
-
     return 0;
 }
+
