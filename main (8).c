132f9fad495825bb1235bd911e67e0de49530b0f
Beacapi <124038376+Beacapi@users.noreply.github.com>
2023-02-02 16:54:58 +0100

    Add files via upload

--- main (8).c
+++ main (8).c
@@ -1,23 +1,35 @@
 /******************************************************************************
 
 Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
 
-int main() {
-int numero;
-numero=100;
+void ft_ft(int n1, int n2, int*quoziente, int*resto) {
+    *quoziente=n1/n2;
+    *resto=n1%n2;
+    }
 
-while (numero >=0) 
-{
-    printf ("%d\n", numero);
-    numero=numero - 2;
-
-}
-
-return 0;
+int main()
+{   int n1 = 0;
+    int n2 = 0;
+    int quoziente;
+    int resto;
+    
+    scanf ("%d",&n1);
+    printf ("inserire un dividendo%d\n", n1);
+    scanf ("%d",&n2);
+    printf ("inserire un divisore%d\n", n2);
+    
+    
+    ft_ft (n1,n2,&quoziente,&resto);
+    printf ("finale %d\n", quoziente);
+    printf ("finale %d\n", resto);
+    
+    
+    
+    return 0;
 }
